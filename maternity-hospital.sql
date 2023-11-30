802bff9458ed2d46ceeb6f84e74293ab58d68417
Andrey Shpak <anshpak.by@gmail.com>
2023-12-01 01:15:10 +0300

    feat: fix normal forms in those databases

--- maternity-hospital.sql
+++ maternity-hospital.sql
@@ -6,12 +6,12 @@ create table hospital(
 	emp_id int unsigned primary key,
     parent_first_name varchar(20),
 	parent_last_name varchar(20),
-    childrens_names varchar(50),
-    childrens_birthdays varchar(50)
+    child_name varchar(50),
+    child_birthday varchar(50)
 );
 
 insert into hospital
-(emp_id, parent_first_name, parent_last_name, childrens_names, childrens_birthdays)
+(emp_id, parent_first_name, parent_last_name, child_name, child_birthday)
 values
 (1001, 'Jane', 'Doe', 'Mary, Sam', '1/1/92, 5/15/94'),
 (1002, 'John', 'Doe', 'Mary, Sam', '1/1/92, 5/15/94'),
@@ -21,217 +21,79 @@ values
 
 select * from hospital;
 
-# Сначала нужно разобраться с родителями, у них есть нарушение 2НФ.
-# Придется создать таблицу для имени, фамилии, а потом и для родителя.
-# Создам и заполню по порядку.
-
 #------------------------------------------------------------------------------------------
-
-# Таблица имен.
-
-drop table if exists first_names;
-create table first_names(
-	first_name_id tinyint unsigned primary key auto_increment,
-    first_name varchar(10)
-);
-
-insert into first_names
-(first_name)
-select parent_first_name from hospital;
 
 set sql_safe_updates = 0;
 
-delete from first_names where first_name_id > 2;
-
-select * from first_names;
-
-#------------------------------------------------------------------------------------------
-
-# Таблица фамилий.
-
-drop table if exists last_names;
-create table last_names(
-	last_name_id tinyint unsigned primary key auto_increment,
-    last_name varchar(30)
-);
-
-insert into last_names
-(last_name)
-select parent_last_name from hospital;
-
-delete from last_names where last_name_id in (2, 4);
-
-select * from last_names;
-
-#------------------------------------------------------------------------------------------
-
-# Таблица связи имен и фамилий родителей.
-
 drop table if exists parents;
 create table parents(
-    first_name_id tinyint unsigned,
-    last_name_id tinyint unsigned,
-    primary key(first_name_id, last_name_id),
-    constraint cn1 foreign key (first_name_id) references first_names(first_name_id),
-    constraint cn2 foreign key (last_name_id) references last_names(last_name_id)
+	parent_id int unsigned,
+    first_name varchar(10),
+    last_name varchar(10),
+    primary key(parent_id)
 );
 
 insert into parents
-(first_name_id, last_name_id)
-values
-(1, 1),
-(1, 3),
-(1, 5),
-(2, 1),
-(2, 3);
+(parent_id, first_name, last_name)
+select emp_id, parent_first_name, parent_last_name from hospital;
 
 select * from parents;
 
-#------------------------------------------------------------------------------------------
-
-# Теперь создам отдельную таблицу для имен детей, дней рождений и свяжу их.
-
 drop table if exists children;
 create table children(
-	child_id tinyint unsigned primary key auto_increment,
-    child_name varchar(50)
+    child_name varchar(50),
+    child_birthday date primary key
 );
 
 insert into children
-(child_name)
-select childrens_names from hospital;
+(child_name, child_birthday)
+select distinct substring_index(child_name, ', ', -1), str_to_date(substring_index(child_birthday, ', ', -1), '%m/%d/%y') from hospital;
 
-delete from children where child_id = 1;
+insert ignore into children
+(child_name, child_birthday)
+select distinct substring_index(child_name, ', ', 1), str_to_date(substring_index(child_birthday, ', ', 1), '%m/%d/%y') from hospital 
+where length(child_name) - length(replace(child_name, ',', '')) + 1 >= 2;
 
-insert into children
-(child_name)
-select substring_index(child_name, ', ', 1) from children where child_id = 2;
+insert ignore into children
+(child_name, child_birthday)
+select distinct substring_index(substring_index(child_name, ', ', 2), ', ', -1), str_to_date(substring_index(substring_index(child_birthday, ', ', 2), ', ', -1), '%m/%d/%y') from hospital
+where length(child_name) - length(replace(child_name, ',', '')) + 1 = 4;
 
-update children
-set child_name = substring_index(child_name, ', ', -1) where child_id = 2;
-
-insert into children
-(child_name)
-select substring_index(child_name, ', ', 1) from children where child_id = 3;
-
-update children
-set child_name = substring(child_name, char_length(substring_index(child_name, ', ', 1)) + 3, char_length(child_name)),
-child_name = substring(child_name, 1, char_length(child_name) - char_length(substring_index(child_name, ', ', -1)) - 2) where child_id = 3;
-
-insert into children
-(child_name)
-select substring_index(child_name, ', ', -1) from children where child_id in (3, 5);
-
-update children
-set child_name = substring_index(child_name, ', ', 1) where child_id in (3, 5);
+insert ignore into children
+(child_name, child_birthday)
+select distinct substring_index(substring_index(child_name, ', ', -2), ', ', 1), str_to_date(substring_index(substring_index(child_birthday, ', ', -2), ', ', 1), '%m/%d/%y') from hospital
+where length(child_name) - length(replace(child_name, ',', '')) + 1 = 4;
 
 select * from children;
 
 #------------------------------------------------------------------------------------------
 
-# Дни рождения
-
-drop table if exists birthdays;
-create table birthdays(
-	birthday_id tinyint unsigned primary key auto_increment,
-    temp_date varchar(50),
-    birthday date
-);
-
-insert into birthdays
-(temp_date)
-select childrens_birthdays from hospital;
-
-delete from birthdays where birthday_id = 2;
-
-insert into birthdays
-(birthday)
-select str_to_date(substring_index(temp_date, ', ', -1), '%m/%d/%y') from birthdays where birthday_id in (1, 3, 5);
-
-update birthdays 
-set temp_date = substring_index(temp_date, ', ', 1) where birthday_id in (1, 5);
-
-insert into birthdays
-(birthday)
-select str_to_date(temp_date, '%m/%d/%y') from birthdays where birthday_id in (1, 4, 5);
-
-insert into birthdays
-(birthday)
-select str_to_date(substring_index(temp_date, ', ', 1), '%m/%d/%y') from birthdays where birthday_id = 3;
-
-update birthdays
-set temp_date = substring(temp_date, char_length(substring_index(temp_date, ', ', 1)) + 3, char_length(temp_date) - char_length(substring_index(temp_date, ', ', 1)) - char_length(substring_index(temp_date, ', ', -1)) - 4) where birthday_id = 3;
-
-insert into birthdays
-(birthday)
-select str_to_date(substring_index(temp_date, ', ', 1), '%m/%d/%y') from birthdays
-where birthday_id = 3;
-
-insert into birthdays
-(birthday)
-select str_to_date(substring_index(temp_date, ', ', -1), '%m/%d/%y') from birthdays
-where birthday_id = 3;
-
-alter table birthdays
-drop column temp_date;
-
-delete from birthdays where birthday_id < 8;
-
-select * from birthdays;
-
-#------------------------------------------------------------------------------------------
-
-# Связь детей и дней рождений
-
-drop table if exists child_cards;
-create table child_cards(
-	child_id tinyint unsigned,
-    birthday_id tinyint unsigned,
-    primary key(child_id, birthday_id),
-    constraint cn3 foreign key (child_id) references children(child_id),
-    constraint cn4 foreign key (birthday_id) references birthdays(birthday_id)
-);
-
-insert into child_cards
-(child_id, birthday_id)
-values
-(2, 8),
-(3, 15),
-(4, 12),
-(5, 13),
-(8, 11),
-(8, 9),
-(9, 14),
-(10, 16),
-(11, 10);
-
-select * from child_cards;
-
-# Пробую их соединить.
-
 drop table if exists parent_and_child;
 create table parent_and_child(
-	first_name_id tinyint unsigned,
-    last_name_id tinyint unsigned,
-    child_id tinyint unsigned,
-    birthday_id tinyint unsigned,
-    constraint cn5 foreign key (first_name_id, last_name_id) references parents(first_name_id, last_name_id),
-    constraint cn6 foreign key (child_id, birthday_id) references child_cards(child_id, birthday_id)
+	parent_id int unsigned,
+    child_birthday date,
+    primary key(parent_id, child_birthday),
+    constraint cn1 foreign key (parent_id) references parents(parent_id),
+    constraint cn2 foreign key (child_birthday) references children(child_birthday)
 );
 
 insert into parent_and_child
-(first_name_id, last_name_id, child_id, birthday_id)
-values
-(1, 1, 2, 8),
-(1, 1, 8, 9),
-(2, 1, 2, 8),
-(2, 1, 8, 9),
-(1, 3, 9, 14),
-(1, 3, 3, 15),
-(1, 3, 8, 11),
-(1, 3, 10, 16),
-(2, 3, 4, 12),
-(1, 5, 5, 13),
-(1, 5, 11, 10);
+(parent_id, child_birthday)
+select distinct emp_id, str_to_date(substring_index(child_birthday, ', ', -1), '%m/%d/%y') from hospital;
+
+insert into parent_and_child
+(parent_id, child_birthday)
+select distinct emp_id, str_to_date(substring_index(child_birthday, ', ', 1), '%m/%d/%y') from hospital 
+where length(child_name) - length(replace(child_name, ',', '')) + 1 >= 2;
+
+insert into parent_and_child
+(parent_id, child_birthday)
+select distinct emp_id, str_to_date(substring_index(substring_index(child_birthday, ', ', 2), ', ', -1), '%m/%d/%y') from hospital
+where length(child_name) - length(replace(child_name, ',', '')) + 1 = 4;
+
+insert into parent_and_child
+(parent_id, child_birthday)
+select distinct emp_id, str_to_date(substring_index(substring_index(child_birthday, ', ', -2), ', ', 1), '%m/%d/%y') from hospital
+where length(child_name) - length(replace(child_name, ',', '')) + 1 = 4;
 
 select * from parent_and_child;
--- rates.sql
+++ rates.sql
@@ -27,48 +27,28 @@ select * from tracks_timetable_info;
 
 drop table if exists rates;
 create table rates(
-	rate_id tinyint unsigned primary key auto_increment,
-    rate_name varchar(15)
+	track_id tinyint unsigned,
+    rate_name varchar(15),
+    primary key(rate_name)
 );
 
-insert into rates
-(rate_name)
-select rate_type from tracks_timetable_info;
-
-delete from rates where rate_id in (2, 5);
+insert ignore into rates
+(track_id, rate_name)
+select track_id, rate_type from tracks_timetable_info;
 
 select * from rates;
 
+drop table if exists tracks;
 create table tracks(
-	track_id tinyint unsigned,
+	rate_name varchar(15),
     start_time varchar(5),
     end_time varchar(5),
-    primary key (track_id, start_time, end_time)
+    primary key (rate_name, start_time, end_time),
+    constraint cn1 foreign key (rate_name) references rates(rate_name)
 );
 
 insert into tracks
-(track_id, start_time, end_time)
-select track_id, start_time, end_time from tracks_timetable_info;
+(rate_name, start_time, end_time)
+select rate_type, start_time, end_time from tracks_timetable_info;
 
 select * from tracks;
-
-create table tracks_and_rates(
-	track_id tinyint unsigned,
-    start_time varchar(5),
-    end_time varchar(5),
-    rate_id tinyint unsigned,
-    constraint cn1 foreign key (track_id, start_time, end_time) references tracks(track_id, start_time, end_time),
-    constraint cn2 foreign key (rate_id) references rates(rate_id)
-);
-
-insert into tracks_and_rates
-(track_id, start_time, end_time, rate_id)
-values
-(1, '09:30', '10:30', 1),
-(1, '11:00', '12:00', 1),
-(1, '14:00', '15:30', 3),
-(2, '10:00', '11:30', 4),
-(2, '11:30', '13:30', 4),
-(2, '15:00', '16:30', 6);
-
-select * from tracks_and_rates;
